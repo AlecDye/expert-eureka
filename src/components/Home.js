9eb8e93294d747a5b375db7af6c0d92bf38658fc
alec dye <alecdye@gmail.com>
2021-06-15 17:27:01 -0700

    Add catch errors to server request

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,38 @@ import {useState, useEffect} from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null)
+  const [blogs, setBlogs] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    fetch(`http://localhost:8000/blogs`)
-      .then(res => {
-        return res.json()
-      })
-      .then((data) => {
-        setBlogs(data)
-      })
+    setTimeout(() => {
+      fetch(`http://localhost:8000/blogs`)
+        .then(res => {
+          if(!res.ok) {
+            throw Error('Connected to server but could not fetch data from the server!')
+          }
+          return res.json()
+        })
+        .then((data) => {
+          setBlogs(data)
+          setIsLoading(false)
+          setError(null)
+        })
+        // Connection error (user couldn't reach server)
+        .catch(err => {
+          setError(err.message)
+          setIsLoading(false)
+        })
+
+      
+    }, 1000)
   }, [])
 
   return (
     <div>
+      {error && <div>{error}</div>}
+      {isLoading && <div>Fetching blog posts...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   )
